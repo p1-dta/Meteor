129a70836a0ed6ecc968a671b605253aa0d20a7c
dorian.turba <dorian.turba@gmail.com>
2022-01-02 17:08:08 +0100

    update version.py

--- version/Version.py
+++ version/Version.py
@@ -15,4 +15,4 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-VERSION = 'v0.1.1'
+VERSION = 'v0.2'

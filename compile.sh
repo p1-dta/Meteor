0490fdb37a5f3337c7e0223428f88180f212f912
dorian.turba <dorian.turba@gmail.com>
2022-01-02 16:50:30 +0100

    fix pyinstaller compilation script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller -w --add-data settings/settings.json;settings --add-data sets/sets.json;sets --noconfirm ./meteor.py
+pyinstaller -w --add-data "settings/settings.json;settings" --add-data "sets/sets.json;sets" --noconfirm ./meteor.py
